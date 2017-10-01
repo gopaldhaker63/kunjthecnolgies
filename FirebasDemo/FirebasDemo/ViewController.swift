dd77cde0f43fac707641a8347ac8d4c445a2803a
gopaldhaker63 <gopaldhaker63@gmail.com>
2017-10-01 13:44:08 +0530

    fcommit

---
+++ FirebasDemo/FirebasDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebasDemo
+//
+//  Created by Gopal on 10/1/17.
+//  Copyright © 2017 Gopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FirebasDemo/Pods/Target Support Files/Pods-FirebasDemo/Pods-FirebasDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FirebasDemo : NSObject
+@end
+@implementation PodsDummy_Pods_FirebasDemo
+@end
